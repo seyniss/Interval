c998d2568bb059d38d82aae370514b83fb48e37b
seyniss <shepunk2@kbu.ac.kr>
2025-07-31 14:42:29 +0900

    1
    복붙

---
+++ script.js
@@ -0,0 +1,35 @@
+const container = document.querySelector('#container')
+const title = document.querySelector('.title')
+const playBtn = document.querySelector('#play')
+const stopBtn = document.querySelector('#stop')
+
+function getRandomColor(){
+    let hex="#"+Math.random().toString(16).slice(2,8).toUpperCase()
+
+
+    return hex
+}
+
+function bgChange(){
+    let randomColor = getRandomColor();
+    
+    container.style.backgroundColor=randomColor
+    title.textContent=randomColor
+    
+}
+let play = setInterval(bgChange,1000)
+
+
+
+
+playBtn.addEventListener('click',()=>{
+    play = setInterval(bgChange,1000)
+       
+})
+stopBtn.addEventListener('click',()=>{
+    if(!play){
+        setInterval(bgChange,1000)
+    }
+    clearInterval(play)
+    play=null 
+})
